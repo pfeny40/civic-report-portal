e4272e547f42691caab76fc3a08f663f03e3b930
Patel Feni <Pfeni40@gmail.com>
2026-08-06 13:21:28 +0530

    Move image upload to Cloudinary

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,12 +1,12 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/");
-    },
-
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "civic-report-portal",
+        allowed_formats: ["jpg", "jpeg", "png"],
     },
 });
 
